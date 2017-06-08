c5295c28e4f0ccfc9f76950bcaef44f92ef75124
kai18 <kaustubh0163@gmail.com>
2017-06-08 23:57:32 +0530

    Addong Recycler view

--- app/src/main/java/tech/kaustubh/jokercard/MainActivity.java
+++ app/src/main/java/tech/kaustubh/jokercard/MainActivity.java
@@ -2,12 +2,27 @@ package tech.kaustubh.jokercard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView songList = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        songList = (RecyclerView) findViewById(R.id.songList);
+        songList.setLayoutManager(new LinearLayoutManager(this));
+        SongListAdapter adapter = new SongListAdapter();
+        songList.setAdapter(adapter);
+        songList.setItemAnimator(new DefaultItemAnimator());
+        adapter.notifyDataSetChanged();
+        Log.d("at", "main");
+
     }
 }
---
+++ app/src/main/java/tech/kaustubh/jokercard/SongListAdapter.java
@@ -0,0 +1,47 @@
+package tech.kaustubh.jokercard;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by kaustubh on 6/7/17.
+ */
+
+public class SongListAdapter extends RecyclerView.Adapter<SongListAdapter.SongHolder> {
+    @Override
+    public SongHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.songview, parent, false);
+        Log.d("inside", "songlistadpater");
+        return new SongHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(SongHolder holder, int position) {
+        holder.text.setText("Hello");
+        Log.d("inside", "songlistadpater");
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class SongHolder extends RecyclerView.ViewHolder {
+        TextView text;
+
+        public SongHolder(View itemView) {
+            super(itemView);
+            Log.d("inside", "songlistadpater");
+            text = (TextView) itemView.findViewById(R.id.songList);
+        }
+
+
+    }
+
+}
